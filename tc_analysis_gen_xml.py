00381e8dec21e7bc74e21358303b89a3176e4a02
Aric Wang <583241212@qq.com>
2023-12-19 15:04:10 +0800

    Update tc_analysis_gen_xml.py

--- tc_analysis_gen_xml.py
+++ tc_analysis_gen_xml.py
@@ -2,6 +2,14 @@ import sys,os,time
 from junit_xml import TestSuite, TestCase
 
 test_cases = [TestCase('Test1', 'some.class.name', 123.345, 'I am stdout!', 'I am stderr!')]
+
+for item in range(10):
+    tc_tmp = TestCase('TC%d' % item , 'some.class.name', 123.345 + item , 'I am stdout!', 'I am stderr!')
+    if item % 2 == 0:
+        tc_tmp.add_error_info(message="Skipped error", output="I skippäd with an error!")
+        
+    test_cases.append(tc_tmp)
+    
 ts = TestSuite("my test suite", test_cases)
 
 with open('results.xml', 'w') as f:
